b561fa5a68df6eb7f853fa3190eabc0245c99783
Panov Alexey <alexeykapanov@gmail.com>
2021-07-31 13:44:00 +0300

    Add code for failed_jobs table creating.

--- docs/tasks/03/create tables.sql
+++ docs/tasks/03/create tables.sql
@@ -7,3 +7,16 @@ create table if not exists service.password_resets
     );
 
 alter table service.password_resets owner to icarhelper_user;
+
+/* service.failed_jobs */
+create table if not exists service.failed_jobs
+(
+    id bigint not null constraint failed_jobs_pk primary key,
+    connection text not null,
+    queue text not null,
+    payload text not null,
+    exception text not null,
+    failed_at timestamp not null
+);
+
+alter table service.failed_jobs owner to icarhelper_user;
